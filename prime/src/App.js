b091240c10d9a5a3d28da8f9fd39249a663494c2
Purple Team <118991961+purpleteamNSC@users.noreply.github.com>
2023-07-02 03:40:44 +0000

    38

---
+++ prime/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>OLA MUNDO</h1>  
+    </div>
+  );
+}
+
+export default App;
---
+++ prime/src/routes.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+import Filme from './pages/Filme'
+
+function RoutesApp(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={ <Home/> } />
+            </Routes>
+        </BrowserRouter>
+    )
+}
